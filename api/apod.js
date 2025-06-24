94f0384466849ffd06ee4120f21fed2016250537
Navyasri_biruda <bnavyasree.ece.rymec@gmail.com>
2025-06-24 13:10:36 +0100

    2462025

--- api/apod.js
+++ api/apod.js
@@ -1,4 +1,3 @@
-// api/apod.js
 import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
@@ -22,4 +21,4 @@ export default async function handler(req, res) {
     console.error(err);
     res.status(500).json({ error: 'Failed to fetch APOD' });
   }
-}
+} 
